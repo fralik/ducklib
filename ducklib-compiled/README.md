7040043426d7c4f872d634690f72f3fba9109b7b
Vadim Frolov <fralik@gmail.com>
2020-03-30 18:59:35 +0200

    Add code to build wheel package

---
+++ ducklib-compiled/README.md
@@ -0,0 +1,19 @@
+# Compiled version of Ducklib library
+
+This repo mimics how one would created a library with compiled code ready for distribution.
+In order to create the wheel do:
+
+```dos
+python setup.py bdist_wheel
+```
+
+Wheel shall be available in `dist` folder.
+
+## Test that library works
+
+Start duck-server in a separate terminal.
+
+```dos
+pip install ducklib-1.0.0-cp37-cp37m-win_amd64.whl
+python -c "import ducklib.main; ducklib.main.do_something()"
+```
--- ducklib-compiled/setup.py
+++ ducklib-compiled/setup.py
@@ -1,24 +1,40 @@
-from setuptools import setup, find_packages
-from Cython.Build import cythonize
+from setuptools import setup, find_packages, Extension
 
 # Load the package's version number
 about = {}
 with open("src/ducklib/__version__.py") as fo:
     exec(fo.read(), about)
 
+
+ext_modules = [
+    Extension(
+        # such a name ensures, that compiled version is placed correctly in the wheel
+        name='ducklib.auth',
+        sources=["src/ducklib/auth.pyx"],
+        # convert pyx to c++ code:
+        language='c++',
+    )
+]
+
+for e in ext_modules:
+    if e.language == 'c++':
+        e.cython_directives = {"language_level": "3"}
+
 setup(
     name="ducklib",
     version=about["__version__"],
     package_dir={"": "src"},
-    packages=find_packages("src"),
+    packages=find_packages(where="src"),
     python_requires="~=3.7",
-    ext_modules=cythonize("src/ducklib/auth.py"),
+    ext_modules=ext_modules,
+    setup_requires=[
+        "setuptools >= 18.0",
+        "cython == 0.29.16",
+    ],
     install_requires=[
         "requests ~= 2.23.0",
         "envparse ~= 0.2.0",
-	"cython == 0.29.16",
     ],
-    extras_require={
-    },
+    extras_require={},
     package_data={},
 )
