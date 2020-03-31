376cb7fa1498e603c43fb73cc13163c612740529
Vadim Frolov <fralik@gmail.com>
2020-03-31 12:10:42 +0200

    Change the way python is compiled
    * This results in function being of cython type rather than built-in (cython_function_or_method vs builtin_function_or_method)

--- ducklib-compiled/setup.py
+++ ducklib-compiled/setup.py
@@ -1,4 +1,6 @@
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+
 
 # Load the package's version number
 about = {}
@@ -6,27 +8,13 @@ with open("src/ducklib/__version__.py") as fo:
     exec(fo.read(), about)
 
 
-ext_modules = [
-    Extension(
-        # such a name ensures, that compiled version is placed correctly in the wheel
-        name='ducklib.auth',
-        sources=["src/ducklib/auth.pyx"],
-        # convert pyx to c++ code:
-        language='c++',
-    )
-]
-
-for e in ext_modules:
-    if e.language == 'c++':
-        e.cython_directives = {"language_level": "3"}
-
 setup(
     name="ducklib",
     version=about["__version__"],
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     python_requires="~=3.7",
-    ext_modules=ext_modules,
+    ext_modules=cythonize("src/ducklib/auth.py", compiler_directives={"language_level": "3"}, language="c++"),
     setup_requires=[
         "setuptools >= 18.0",
         "cython == 0.29.16",
--- ducklib-compiled/src/ducklib/__init__.py
+++ ducklib-compiled/src/ducklib/__init__.py
@@ -1,1 +1,2 @@
 from . import main
+from .auth import is_user_valid
